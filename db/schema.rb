0bdc0f14c8e2a99688913c67361e858d229e3883
Nick Brown <nickbrown714@gmail.com>
2016-02-01 12:11:45 -0800

    fix missing migrate folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131172601) do
+ActiveRecord::Schema.define(version: 20160131232205) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "dept"
