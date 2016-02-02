98075c4dff59f6c55afcb445fb2f80dcbebccf40
Ishjot Walia <ishjotwalia@umail.ucsb.edu>
2016-02-01 21:44:14 -0800

    finished task 2 on AdminPanel story, still need to test

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -1,22 +1,35 @@
 class AdminPanelController < ApplicationController
-   def list
-   end
+
+  before_filter :authenticate_user!
+  before_filter :admin_only
+
+  def list
+    @users = User.all
+  end
    
-   def show
-   end
+  def show
+  end
    
-   def new
-   end
+  def new
+  end
    
-   def create
-   end
+  def create
+  end
    
-   def edit
-   end
+  def edit
+  end
    
-   def update
-   end
+  def update
+  end
    
-   def delete
-   end
+  def delete
+  end
+
+  private
+
+  def admin_only
+    unless current_user.has_role? :admin
+      redirect_to :back, :alert => "Access denied"
+    end
+  end
 end
