cd2a382c993f8ed529ed78fe8851af6c7e40c93c
Nick Brown <nickbrown714@gmail.com>
2016-02-03 21:56:57 -0800

    adding student-course relationship queries and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-require 'net/https'
 
 class User < ActiveRecord::Base
   rolify
@@ -13,9 +12,6 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.name = auth.info.name
       user.github_token = auth.credentials.token
-#      user.password = Devise.friendly_token[0,20]
-#      user.name = auth.info.name
-#      user.image = auth.info.image
     end
   end
   
@@ -31,10 +27,24 @@ class User < ActiveRecord::Base
     response = http.request(request)
     case response.code.to_i
     when 200 || 201
-      response.body
+      JSON.parse(response.body)
     else
       nil
     end
+  end
+
+  def student_record
+    emails = self.github_emails
+    student = nil
+    unless emails.nil?
+      emails.each do |email|
+        begin
+          student = Student.find_by! email: email["email"]
+        rescue ActiveRecord::RecordNotFound => e
+        end
+      end
+    end
+    return student
   end
 
   def is_admin
--- app/views/students/show.json.jbuilder
+++ app/views/students/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @student, :id, :first_name, :last_name, :email, :studentid, :created_at, :updated_at
+json.extract! @student, :id, :first_name, :last_name, :email, :studentid
